608023552e9997e6cacdc601c0a2bb02db1db74d
lc <lc@lc.com>
2017-08-09 21:52:17 -0400

    various

--- test.py
+++ test.py
@@ -2,8 +2,13 @@
 """ this is testDco"""
 # -*- coding: utf-8 -*-
 
-number = int(input("Enter an integer: "))
-if number < 100:
-    print 'Your number is smaller than 100'
-else:
-    print 'Your number is greater than 100'
+amount = float(input("Enter amount: "))   
+inrate = float(input("Enter Interest rate: "))   
+period = int(input("Enter period: "))   
+value = 0
+year = 1
+while year <= period:
+    value = amount + (inrate * amount)
+    print("Year {} Rs. {:.2f}".format(year, value))
+    amount = value
+    year = year + 1
